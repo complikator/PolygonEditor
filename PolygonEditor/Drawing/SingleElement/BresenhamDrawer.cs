215ded18078c8f288ac5063f5e4b667053c4ad4e
complikator <pawel13156@gmail.com>
2022-10-24 11:02:43 +0200

    fix: Fix bresenham drawing width

--- PolygonEditor/Drawing/SingleElement/BresenhamDrawer.cs
+++ PolygonEditor/Drawing/SingleElement/BresenhamDrawer.cs
@@ -14,50 +14,95 @@ namespace PolygonEditor.Drawing
 
         protected override void drawLine(Edge edge)
         {
-            bresenhamLine(edge.beginning.x, edge.beginning.y, edge.end.x, edge.end.y, defaultPen);
+            Console.WriteLine("bresenham line");
+            bresenhamLine(edge.beginning.x, edge.beginning.y, edge.end.x, edge.end.y, defaultBrush);
         }
 
-        private void bresenhamLine(int x, int y, int x2, int y2, Pen pen)
+        private void bresenhamLine(int x1, int y1, int x2, int y2, Brush brush)
         {
-            int w = x2 - x;
-            int h = y2 - y;
-            int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
-            if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
-            if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
-            if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
-            int longest = Math.Abs(w);
-            int shortest = Math.Abs(h);
-            if (!(longest > shortest))
+            // zmienne pomocnicze
+            int d, dx, dy, ai, bi, xi, yi;
+            int x = x1, y = y1;
+            // ustalenie kierunku rysowania
+            if (x1 < x2)
             {
-                longest = Math.Abs(h);
-                shortest = Math.Abs(w);
-                if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
-                dx2 = 0;
+                xi = 1;
+                dx = x2 - x1;
             }
-            int numerator = longest >> 1;
-            for (int i = 0; i <= longest; i++)
+            else
             {
-                putPixel(x, y, pen);
-                numerator += shortest;
-                if (!(numerator < longest))
+                xi = -1;
+                dx = x1 - x2;
+            }
+            // ustalenie kierunku rysowania
+            if (y1 < y2)
+            {
+                yi = 1;
+                dy = y2 - y1;
+            }
+            else
+            {
+                yi = -1;
+                dy = y1 - y2;
+            }
+            // pierwszy piksel
+            putPixel(x, y, brush);
+            // oś wiodąca OX
+            if (dx > dy)
+            {
+                ai = (dy - dx) * 2;
+                bi = dy * 2;
+                d = bi - dx;
+                // pętla po kolejnych x
+                while (x != x2)
                 {
-                    numerator -= longest;
-                    x += dx1;
-                    y += dy1;
+                    // test współczynnika
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                    }
+                    else
+                    {
+                        d += bi;
+                        x += xi;
+                    }
+                    putPixel(x, y, brush);
                 }
-                else
+            }
+            // oś wiodąca OY
+            else
+            {
+                ai = (dx - dy) * 2;
+                bi = dx * 2;
+                d = bi - dy;
+                // pętla po kolejnych y
+                while (y != y2)
                 {
-                    x += dx2;
-                    y += dy2;
+                    // test współczynnika
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                    }
+                    else
+                    {
+                        d += bi;
+                        y += yi;
+                    }
+                    putPixel(x, y, brush);
                 }
             }
         }
 
-        private void putPixel(int x, int y, Pen pen)
+        private void putPixel(int x, int y, Brush brush)
         {
+            Console.WriteLine($"drawing point: {x}, {y}");
             using (Graphics g = Graphics.FromImage(drawingArea))
             {
-                g.DrawRectangle(pen, x, y, 1, 1);
+                g.FillRectangle(brush, x, y, 1, 1);
             }
         }
     }
